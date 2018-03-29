7c7c6c652bbce1953e1cce16c7076e124b6e8f58
elcarim5efil <elcarim5efil@gmail.com>
2018-03-29 13:40:35 +0800

    add Oxz.middleware method

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,42 @@ import * as Koa from 'koa';
 import Enhancer from './middleware/enhancer';
 import Router from './middleware/router';
 import Proxy from './middleware/proxy';
+import * as compose from 'koa-compose';
 import * as BodyParser from 'koa-bodyparser';
 import { ConfigConstructor, MockConfig } from './helpers/config-constructor';
 
 class Oxz {
   private app: Koa;
+
   run(option: Object) {
     this.app = new Koa();
     Oxz.install(this.app, option);
     this.app.listen(3000);
     console.log('Server running in 3000');
   }
-  static install(app: Koa, option: Object|string) {
-    const config = new ConfigConstructor(option).get()
-    const enableProxy = config.proxy && config.proxy.enable;
 
-    app.use(new Enhancer(config).enhance());
+  static middleware(option: Object|string) {
+    const config = new ConfigConstructor(option).get();
+    const enableProxy = config.proxy && config.proxy.enable;
+    const middlewares = [];
+
+    middlewares.push(new Enhancer(config).enhance());
 
     if (!enableProxy) {
-      app.use(BodyParser());
-      app.use(new Router().routes());
+      middlewares.push(BodyParser());
+      middlewares.push(new Router().routes());
     } else {
-      app.use(new Proxy().proxy());
+      middlewares.push(new Proxy().proxy());
     }
+
+    return compose(middlewares)
+  }
+
+  static install(app: Koa, option: Object|string) {
+    app.use(Oxz.middleware(option));
   }
 }
 
-new Oxz().run(require('../example/config'));
+// new Oxz().run(require('../test/example/config'));
 
 module.exports = Oxz;
